352c95e1752976ee81b21510ac743d1e16940edb
ElisreginaL <144842307+ElisreginaL@users.noreply.github.com>
2023-09-12 18:38:38 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+var apartamentos = window.document.getElementById("imagem")
+function AlterarAp() {
+    imagem.src="imagens/2-decoração-loft-industrial-com-mezanino_resized.jpg"
+}
+function AlterarAp2() {
+    imagem.src="imagens/apartamento.svg"
+}
